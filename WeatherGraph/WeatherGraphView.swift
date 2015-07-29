61e20429bd0e0d5adf7a6cbbb5fe0e66d49a3199
Jens Bruggemans <Jens@Jenss-MacBook-Pro.local>
2015-07-29 16:02:57 +0200

    Added WeatherGraphView.

---
+++ WeatherGraph/WeatherGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherGraphView.swift
+//  WeatherGraph
+//
+//  Created by Jens Bruggemans on 29/07/15.
+//  Copyright (c) 2015 Embur. All rights reserved.
+//
+
+import UIKit
+
+class WeatherGraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
