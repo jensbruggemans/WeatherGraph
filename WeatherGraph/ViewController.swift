24bfd1ac734c4eb2a1938c9d1174016c8dbf0e80
Jens Bruggemans <Jens@Jenss-MacBook-Pro.local>
2015-07-29 14:47:55 +0200

    Initial Commit

---
+++ WeatherGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherGraph
+//
+//  Created by Jens Bruggemans on 29/07/15.
+//  Copyright (c) 2015 Embur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
